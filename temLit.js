67e9c1583763d32ea44ec72f905e96710980388e
JorgeCollazo <jorgecollazojr541@gmail.com>
2020-09-23 15:22:18 -0700

    removed unnecessary parentheses

--- temLit.js
+++ temLit.js
@@ -1,3 +1,4 @@
+
 // Name:Jorge Collazo
 // Date:9-23-2020
 // Assignment: Template Literal Practice
@@ -18,32 +19,32 @@ lastName;
 // Create a variable that will hold a score and a var that will hold a full name 
 
 //let score = 0;
-let score = (`${0}`);
+let score = `${0}`;
 
 //let fullName = firstName + " " + lastName;
-let fullName = (`${firstName}  ${lastName}`);
+let fullName = `${firstName}  ${lastName}`;
 fullName;
 
 // create a var that adds the full name var and score in a message
 
 //let highscore = fullName + " " + "has a score of " + score;
-let highscore = (`${fullName} has a score of ${score}`);
+let highscore = `${fullName} has a score of ${score}`;
 highscore;
 
 ////////////////////////Pt. 2////////////////////////
 //comment out the lines above then create two var's that hold numbers
 
 //let num1 = 2;
-let num1 = (`${2}`);
+let num1 = `${2}`;
 //let num2 = 1;
-let num2 = (`${1}`);
+let num2 = `${1}`;
 num1;
 num2;
 
 //create an add var that adds both vars
 
 //let sum = num1 + num2;
-let sum = (`${num1} ${num2}`);
+let sum = `${num1} ${num2}`;
 sum;
 
 //console log the newly created add var
